d925977020edd41e548ff697bf88a6b57b1d3fe3
pokoa2 <108578607+pokoa2@users.noreply.github.com>
2022-07-02 20:13:18 +0700

    Update line_notify_bot.py

--- line_notify_bot.py
+++ line_notify_bot.py
@@ -29,7 +29,7 @@ print("Current Time =", current_time)
 print("Your Time Zone is GMT", strftime("%z", gmtime()))
 
 str1 = "\n13:00 - 16:00 \n\nITCS343 (Online)\nPrinciples of Operating Systems\n\n [https://teams.microsoft.com/l/channel/19%3aa7a036c206254a7c9b1801ea40d348f1%40thread.tacv2/Section%25202?groupId=96da9da8-9ea8-492d-a5b3-554b896da588&tenantId=9bc585f9-a8b8-431c-9013-efa7b2b40cdf]"
-lineNotify("Testing Bot. . .")
+lineNotify("Testing Bot. .")
 
 keep_alive()
 
