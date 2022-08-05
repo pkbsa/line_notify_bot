c217eec2509e48ee4fa748a63c7cc86c61ea521b
pkbsa <siranut.poko@hotmail.com>
2022-08-05 21:30:06 +0700

    Setup Schedule

--- line_notify_bot.py
+++ line_notify_bot.py
@@ -18,7 +18,10 @@ def notifySticker(stickerID,stickerPackageID):
 def _lineNotify(payload,file=None):
     import requests
     url = 'https://notify-api.line.me/api/notify'
-    token = 'urI4RcOBlRjV0wYCcHsa742KGrRVcikcuE4mxfHVlld'
+    #Sec2 
+    token = 'AluZ73jnEqlv9qUf43hXbDLIN21DYUMurYRIrt1kusJ'
+    #Testbot
+    #token = 'Eowx3XT2qK5FJf0PQ5BRdFJM80b5hIDx6l55OZ4uEOn'
     headers = {'Authorization':'Bearer '+token}
     return requests.post(url, headers=headers , data = payload, files=file)
 
@@ -28,10 +31,16 @@ current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 print("Your Time Zone is GMT", strftime("%z", gmtime()))
 
-str1 = "\n13:00 - 16:00 \n\nITCS343 (Online)\nPrinciples of Operating Systems\n\n [https://teams.microsoft.com/l/channel/19%3aa7a036c206254a7c9b1801ea40d348f1%40thread.tacv2/Section%25202?groupId=96da9da8-9ea8-492d-a5b3-554b896da588&tenantId=9bc585f9-a8b8-431c-9013-efa7b2b40cdf]"
-lineNotify("Testing Bot. .")
+strMon_1 = "\n9:00 - 12:00 \n\nITLG301 (On-site)\nBusiness Writing\n\n2A : Aj.Paul (Room: IT305)\n2B : Aj.Karl (Room: IT312)\n2C : Aj.Peter C. (Room: IT322)"
+strMon_2 = "\n13:00 - 16:00 \n\nITCS451 (On-site)\nArtificial Intelligence\n\nAj.Peter / Aj.Thanapon / Aj.Apirak\n\nRoom: IT322"
+strTue_1 = "\n9:00 - 12:00 \n\nITCS361 (On-site)\nManagement Information System\n\nAj.Songsri\n\nRoom: IT322"
+strTue_2 = "\n13:00 - 16:00 \n\nITCS420 (On-site)\nComputer Networks\n\nAj.Vasaka / Aj.Assadarat / Aj.Dolvara\n\nRoom: IT322"
+strWed_1 = "\n9:00 - 12:00 \n\nITCS414 (On-site)\nInformation Storage and Retrieval\n\nAj.Suppawong\n\nRoom: IT322"
+strThu_1 = "\n9:00 - 12:00 \n\nITCS443 (On-site)\nParallel and Distributed Systems\n\nAj.Ekasit\n\nRoom: IT322"
+strFri_1 = "\n9:00 - 12:00 \n\nITCS371 (On-site)\nIntroduction to Software Engineering\n\nAj.Thanwadee / Aj.Chaiyong / Aj.Morakot / Aj.Wudhichart\n\nRoom: IT305"
 
 keep_alive()
+lineNotify("...Start Line Notify")
 
 while 1 :
     now = datetime.now(timezone('Asia/Bangkok'))
@@ -41,22 +50,25 @@ while 1 :
 
     #monday
     if date.today().weekday() == 0 : 
-        if (current_time == "9:00:00") :
-            print("stop")
+        if (current_time == "08:45:00") :
+            lineNotify(strMon_1)
+        if (current_time == "12:45:00") :
+            lineNotify(strMon_2)
     #tuesday
     if date.today().weekday() == 1 : 
-        if (current_time == "9:00:00") :
-            print("stop")
+        if (current_time == "08:45:00") :
+            lineNotify(strTue_1)
+        if (current_time == "12:45:00") :
+            lineNotify(strTue_2)
     #wednesday
     if date.today().weekday() == 2 : 
-        if (current_time == "9:00:00") :
-            print("stop")
+        if (current_time == "08:45:00") :
+            lineNotify(strWed_1)
     #thursday
     if date.today().weekday() == 3 : 
-        if (current_time == "19:52:55") :
-            lineNotify(str1)
+        if (current_time == "08:45:00") :
+            lineNotify(strThu_1)
     #friday
     if date.today().weekday() == 4 : 
-        if (current_time == "19:48:30") :
-            print("stop")
-
+        if (current_time == "08:45:00") :
+            lineNotify(strFri_1)
